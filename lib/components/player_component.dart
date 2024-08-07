635416403fb4fb5d5b3eea500500cd4de9cbe968
Kiishi David <thatsaxydev@gmail.com>
2024-08-07 19:11:42 +0100

    sprites added

--- lib/components/player_component.dart
+++ lib/components/player_component.dart
@@ -4,13 +4,14 @@ import 'dart:math';
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flame/sprite.dart';
 import 'package:flame_audio/flame_audio.dart';
 import 'package:flutter/material.dart';
 import 'package:regun/components/border_component.dart';
 import 'package:regun/components/enemy_component.dart';
 import 'package:regun/my_game.dart';
 
-class PlayerComponent extends PositionComponent
+class PlayerComponent extends SpriteAnimationComponent
     with HasGameReference<RegunGame>, CollisionCallbacks {
   PlayerComponent({
     super.position,
@@ -25,14 +26,11 @@ class PlayerComponent extends PositionComponent
     ..style = PaintingStyle.stroke
     ..strokeCap = StrokeCap.round
     ..strokeWidth = 10;
-  static final _trailPaint = Paint()
-    ..color = Colors.red.withOpacity(0.5)
-    ..strokeWidth = 2.0
-    ..style = PaintingStyle.stroke;
-  // late final SpawnComponent _bulletSpawner;
   double maxSpeed = 300.0;
   late final Vector2 _lastSize = size.clone();
   late final Transform2D _lastTransform = transform.clone();
+  late SpriteAnimation idleAnimation;
+  late SpriteAnimation runAnimation;
 
   //!
   Random random = Random();
@@ -42,6 +40,25 @@ class PlayerComponent extends PositionComponent
   @override
   Future<void> onLoad() async {
     await super.onLoad();
+    final idleSpriteSheet = SpriteSheet(
+      image: await game.images.load('player_idle.png'),
+      srcSize: Vector2(48, 48),
+    );
+    final runSpriteSheet = SpriteSheet(
+      image: await game.images.load('player_run.png'),
+      srcSize: Vector2(48, 48),
+    );
+    idleAnimation = idleSpriteSheet.createAnimation(
+      row: 0,
+      stepTime: 0.5,
+      to: 5,
+    );
+    runAnimation = runSpriteSheet.createAnimation(
+      row: 0,
+      stepTime: 0.5,
+      to: 6,
+    );
+    animation = runAnimation;
     add(
       CircleHitbox(
         radius: playerRadius,
@@ -58,73 +75,48 @@ class PlayerComponent extends PositionComponent
       _lastSize.setFrom(size);
       _lastTransform.setFrom(transform);
       position.add(game.movementJoystick.relativeDelta * maxSpeed * dt);
-      angle = game.movementJoystick.delta.screenAngle();
-
-      //
-      // add(
-      //   ParticleSystemComponent(
-      //     position: position.clone(),
-      //     particle: Particle.generate(
-      //       count: 40,
-      //       generator: (i) {
-      //         return AcceleratedParticle(
-      //           lifespan: 2,
-      //           speed: Vector2(
-      //                 noise.transform(random.nextDouble()),
-      //                 noise.transform(random.nextDouble()),
-      //               ) *
-      //               i.toDouble(),
-      //           child: CircleParticle(
-      //             radius: 2,
-      //             paint: Paint()
-      //               ..color = colorTween.transform(random.nextDouble())!,
-      //           ),
-      //         );
-      //       },
-      //     ),
-      //   ),
-      // );
+      // angle = game.movementJoystick.delta.screenAngle();
     }
   }
 
   @override
   void onMount() {
-    debugMode = true;
-    size = Vector2.all(playerRadius * 2);
+    // debugMode = true;
+    size = Vector2.all(playerRadius * 4);
     anchor = Anchor.center;
     super.onMount();
   }
 
-  @override
-  void render(Canvas canvas) {
-    // canvas.drawCircle(
-    //   (size / 2).toOffset(),
-    //   playerRadius,
-    //   _paint,
-    // );
-    final halfSize = playerRadius;
+  // @override
+  // void render(Canvas canvas) {
+  //   // canvas.drawCircle(
+  //   //   (size / 2).toOffset(),
+  //   //   playerRadius,
+  //   //   _paint,
+  //   // );
+  //   final halfSize = playerRadius;
 
-    // define the path for a centered triangle
-    final path = Path()
-      ..moveTo(0, -halfSize) // top vertex
-      ..lineTo(-halfSize, halfSize) // Bottom-left vertex
-      ..lineTo(0, halfSize - 12) // center vertex
-      ..lineTo(halfSize, halfSize) // Bottom-right vertex
-      ..close();
+  //   // define the path for a centered triangle
+  //   final path = Path()
+  //     ..moveTo(0, -halfSize) // top vertex
+  //     ..lineTo(-halfSize, halfSize) // Bottom-left vertex
+  //     ..lineTo(0, halfSize - 12) // center vertex
+  //     ..lineTo(halfSize, halfSize) // Bottom-right vertex
+  //     ..close();
 
-    // translate canvas to center the triangle
-    canvas.save();
-    canvas.translate(size.x / 2, size.y / 2);
+  //   // translate canvas to center the triangle
+  //   canvas.save();
+  //   canvas.translate(size.x / 2, size.y / 2);
 
-    // Draw the triangle
-    canvas.drawPath(
-      path,
-      _paint,
-    );
+  //   // Draw the triangle
+  //   canvas.drawPath(
+  //     path,
+  //     _paint,
+  //   );
 
-    // Restore the canvas to its original state
-    canvas.restore();
-  }
+  //   // Restore the canvas to its original state
+  //   canvas.restore();
+  // }
 
   @override
   void onCollisionStart(
